2fc0d129924dbb2f714b49d8315907440185dbe1
LuckyYunicorn <lucky.parihar@yunicorn.in>
2025-09-23 18:06:50 +0530

    login updates added my name

--- lib/otp_screen.dart
+++ lib/otp_screen.dart
@@ -10,6 +10,6 @@ class OtpScreen extends StatefulWidget {
 class _OtpScreenState extends State<OtpScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Lucky Singh Parihar "),),);
+    return Scaffold(appBar: AppBar(title: Text("Lucky Parihar "),),);
   }
 }
