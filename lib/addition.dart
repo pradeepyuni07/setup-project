5e6fc6bb47ee76fc406d9e1f090458558dda9916
Bablu <Bablu.dhakad@yunicorn.in>
2025-09-23 17:17:35 +0530

    addition file

--- lib/addition.dart
+++ lib/addition.dart
@@ -1,1 +1,10 @@
-// add some data
+import 'package:flutter/material.dart';
+
+class Addition extends StatelessWidget {
+  const Addition({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
